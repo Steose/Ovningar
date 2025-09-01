2a4410aff3b16f9648889fd09ca8184785ef3c7c
Steose <60steose@gafe.molndal.se>
2025-09-02 00:05:59 +0200

    commit

--- helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
+++ helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769995dea55ff3575fda8d7d1de03d8fcf838bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a10a326686e9ea63e987687f2fa31996528fe52")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

011a6dcd9fe9dac7ee31174d35e27c37eafe7cfb
60steose@gafe.molndal.se <60steose@gafe.molndal.se>
2025-09-01 11:05:17 +0200

    ignoring

--- helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
+++ helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951731d2cd79df4040e0677fef304fc8dff25c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
